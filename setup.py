bab5e2105fca4916e0389057010dc832773a47c6
Aliha <alihaather.off.com>
2026-03-12 10:10:10 +0500

    feat: add real build to pipeline

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="calculator",
+    version="1.0.0",
+    author="Aliha",
+    description="A simple Python calculator",
+    packages=find_packages(),
+    python_requires=">=3.10",
+)
